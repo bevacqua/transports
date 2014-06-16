28dcf4da6745fe4239270e8282a2a2d1a4918a57
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-06-16 20:52:38 -0300

    improving

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -2,43 +2,14 @@
 
 var _ = require('lodash');
 var passport = require('passport');
-var authenticationOptions = {
-  failureRedirect: data.login,
-  failureFlash: true
-};
-var login = passport.authenticate('local', authenticationOptions);
-
-function rememberReturnUrl (req, res, next) {
-  req.session.redirect = req.query.redirect;
-  next();
-}
-
-function redirect (req, res) {
-  var sessionRedirect = req.session.redirect;
-  delete req.session.redirect;
-  res.redirect(req.body.redirect || sessionRedirect || data.success);
-}
-
-function requireAnonymous (req, res, next) {
-  if (req.user) {
-    redirect(req,res);
-  }
-  next();
-}
-
-function logout (req, res) {
-  req.logout();
-  res.redirect('/');
-}
-
-function callbacks (name, options) {
-  return {
-    auth: passport.authenticate(name, options),
-    callback: passport.authenticate(name, authenticationOptions)
-  };
-}
 
 function routing (data, app, register) {
+  var authenticationOptions = {
+    failureRedirect: data.login,
+    failureFlash: true
+  };
+  var login = passport.authenticate('local', authenticationOptions);
+
   app.get(data.logout, logout);
   app.get(data.login, requireAnonymous);
 
@@ -48,8 +19,8 @@ function routing (data, app, register) {
   _.keys(data.providers).forEach(setup);
 
   function setup (name) {
-    var provider = callbacks(name, data.providers[name].options);
-    var meta = data[name];
+    var meta = data.providers[name];
+    var provider = callbacks(name, meta.options);
     if (!meta.enabled) {
       return;
     }
@@ -63,6 +34,36 @@ function routing (data, app, register) {
     } else {
       register(req, res, next);
     }
+  }
+
+  function rememberReturnUrl (req, res, next) {
+    req.session.redirect = req.query.redirect;
+    next();
+  }
+
+  function redirect (req, res) {
+    var sessionRedirect = req.session.redirect;
+    delete req.session.redirect;
+    res.redirect(req.body.redirect || sessionRedirect || data.success);
+  }
+
+  function requireAnonymous (req, res, next) {
+    if (req.user) {
+      redirect(req,res);
+    }
+    next();
+  }
+
+  function logout (req, res) {
+    req.logout();
+    res.redirect('/');
+  }
+
+  function callbacks (name, options) {
+    return {
+      auth: passport.authenticate(name, options),
+      callback: passport.authenticate(name, authenticationOptions)
+    };
   }
 }
 
--- transports.js
+++ transports.js
@@ -1,31 +1,12 @@
 'use strict';
 
 var _ = require('lodash');
-var passport = require('passport');
+var passports = require('./lib/passports');
 var serialization = require('./lib/serialization');
 var routing = require('./controllers/authentication');
 
-function initialize (data, providerHandler) {
-  var oauth1 = require('./lib/oauth1')(data, providerHandler);
-  var oauth2 = require('./lib/oauth2')(data, providerHandler);
-  var openid = require('./lib/openid')(data, providerHandler);
-  var protocols {
-    oauth1: oauth1,
-    oauth2: oauth2,
-    openid: openid
-  };
-
-  _.keys(data.providers).forEach(function setup (name) {
-    var provider = data.providers[name];
-    var protocol = protocols[provider.protocol];
-    if (!protocol) {
-      throw new Error(provider.protocol + ' is unsupported');
-    }
-    protocol(name, provider.strategy, provider.fields);
-  });
-}
-
-var api = module.exports = initialize;
-
-api.serialization = serialization;
-api.routing = routing;
+module.exports = {
+  passports: passports,
+  serialization: serialization,
+  routing: routing
+};
